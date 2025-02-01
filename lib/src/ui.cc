5888922d62183e420950d6124bea2ff7c319d868
Andre Wagner <WagnerAndre@JohnDeere.com>
2025-02-01 15:53:52 -0600

    .

--- lib/src/ui.cc
+++ lib/src/ui.cc
@@ -1,1 +1,6 @@
 #include "ui.hh"
+
+UI::UI()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+}
--- lib/src/ui.hh
+++ lib/src/ui.hh
@@ -1,10 +1,11 @@
 #ifndef UI_HH
 #define UI_HH
 
-// #include <SDL3/SDL.h>
+#include <SDL3/SDL.h>
 
 class UI {
-
+public:
+    UI();
 };
 
 #endif //UI_HH
